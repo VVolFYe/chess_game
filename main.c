f6f48787c837ae5a7309b4dc47f6bc44246b7d18
VVolFYe <tudor.boscu@gmail.com>
2025-03-29 22:45:02 +0200

    committed

--- main.c
+++ main.c
@@ -3,13 +3,22 @@
 #include <SDL2/SDL_test_images.h>
 #include <SDL2/SDL_ttf.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 800
 
+// PAGINA DE MENIU
+
+typedef struct{
+    SDL_Rect rect;      // forma
+    const char *label;  // display-text
+}
+
 int main(void)
 {
-    printf("lalalala 10 32.\n");
+    printf("Start.\n");
+
 
 
 
