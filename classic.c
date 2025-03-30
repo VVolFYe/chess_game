3d519aac76976e133e3daa661fddc2755d501932
VVolFYe <tudor.boscu@gmail.com>
2025-03-30 23:40:29 +0300

    commited

--- classic.c
+++ classic.c
@@ -10,6 +10,7 @@
 SDL_Window *classic_window = NULL;
 SDL_Renderer *classic_renderer = NULL;
 TTF_Font *classic_font = NULL;
+SDL_Texture *chessboard = NULL;
 
 bool init_classic() {
     classic_window = SDL_CreateWindow("Classic Chess", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
@@ -20,14 +21,28 @@ bool init_classic() {
 
     classic_renderer = SDL_CreateRenderer(classic_window, -1, SDL_RENDERER_ACCELERATED);
     if (!classic_renderer) {
-        fprintf(stderr, "Failed to create Classic Chess renderer: %s\n", SDL_GetError());
+        fprintf(stderr, "Failed to create Classic Chess renderer: %s\n", SDL_GetError());  //afisam eroarea cu geterror (sdl)
         SDL_DestroyWindow(classic_window);
         return false;
     }
 
     classic_font = TTF_OpenFont("arial.ttf", 24); // Load the font with size 24
         if (!classic_font) {
-        fprintf(stderr, "Failed to load font: %s\n", TTF_GetError());
+        fprintf(stderr, "Failed to load font: %s\n", TTF_GetError());  //afisam eroare cu geterror (ttf)
+        return false;
+    }
+
+    SDL_Surface *background_surface = IMG_Load("chessboard.jpg");  // Replace with the path to your image
+    if (!background_surface) {
+        fprintf(stderr, "Failed to load chessboard: %s\n", SDL_GetError());
+        return false;
+    }
+
+    chessboard = SDL_CreateTextureFromSurface(classic_renderer, background_surface);
+    SDL_FreeSurface(background_surface);  // Free the surface after creating the texture
+
+    if (!chessboard) {
+        fprintf(stderr, "Failed to create background texture: %s\n", SDL_GetError());
         return false;
     }
 
@@ -35,6 +50,7 @@ bool init_classic() {
 }
 
 void close_classic() {
+    SDL_DestroyTexture(chessboard);
     SDL_DestroyRenderer(classic_renderer);
     SDL_DestroyWindow(classic_window);
 }
